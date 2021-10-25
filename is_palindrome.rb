330827453b3007ccde9e32b972139a698edf706b
Kevin Khau <kevin.khau@simplebo.fr>
2021-10-25 21:05:23 +0200

    Unit tests

--- is_palindrome.rb
+++ is_palindrome.rb
@@ -2,4 +2,28 @@ def is_palindrome(word)
   false
 end
 
-is_palindrome
+@correct = 0
+@count = 0
+def test_is_palindrome
+  handle_test("aba", true)
+  handle_test("momo", false)
+  handle_test("rail at a liar", true)
+  handle_test("", true)
+  handle_test("Simplébo", false)
+  handle_test("Engage le jeu que je le gagne", true)
+  handle_test("Tâte l'État", true)
+  puts "Tests: #{@correct} / #{@count}"
+end
+
+def handle_test(expression, expected)
+  @count += 1
+  puts "Test #{@count} with expression: '#{expression}'"
+  actual = is_palindrome(expression)
+  if actual != expected
+    puts "x Expected #{expected}, got #{actual}"
+  else
+    @correct += 1
+  end
+end
+
+test_is_palindrome
